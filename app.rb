5943eac6030f997c830a2afad84b072dd707a781
Pavel Gordienko <guzmo6@rambler.ru>
2016-02-10 14:37:32 +0300

    add details.erb

--- app.rb
+++ app.rb
@@ -48,9 +48,9 @@ post '/new' do
 end
 
 # вывод информации о посте
-
 get '/details/:post_id' do
 	post_id = params[:post_id]
-
-	erb "Post id #{post_id}" 
+	results = @db.execute 'select * from Posts where id = ?', [post_id] 
+	@row = results[0]
+	erb :details
 end
